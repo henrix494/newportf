ff16267a0273ec1b52aa8017e5d6ec96bd381b68
henrix494 <henrix494@gmail.com>
2024-05-18 18:50:54 +0300

    added some style to hero and project

--- components/hero/heroBIGText/Text.tsx
+++ components/hero/heroBIGText/Text.tsx
@@ -23,7 +23,7 @@ export default function Text() {
   return (
     <div
       ref={containerRef}
-      className="absolute bottom-[-10%] lg:bottom-[-5%] left-1/2 translate-x-[-48%]  lg:translate-x-[-50%] lg:text-[28rem] text-[16rem] text-white "
+      className="absolute bottom-[-10%] lg:bottom-[-5%] left-1/2 translate-x-[-48%]  lg:translate-x-[-50%] xl:text-[28rem] text-[16rem] text-white "
     >
       <div className="flex gap-60 text-[#de704d] box opacity-0 max-lg:rotate-90 h-res">
         <p>נתן</p> מינקוב
